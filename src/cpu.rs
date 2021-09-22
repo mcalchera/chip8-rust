b9f8d6890eb7112b0a25bc27d2512af1ba9b8870
Mark Calchera <mcalchera@gmail.com>
2021-09-22 18:08:52 -0500

    add tests for: - op2nnn - 3xnn - 4xnn - 5xy0 - 6xnn - 7xnn - 8xy0 - 8xy1 - 8xy2 - 8xy3 - 8xy3 - 8xy5 - 8xy4

--- src/cpu.rs
+++ src/cpu.rs
@@ -140,7 +140,7 @@ impl Cpu {
             0x2 => self.op_2nnn(),
             0x3 => self.op_3xnn(),
             0x4 => self.op_4xnn(),
-            0x5 => self.op_5xyn(),
+            0x5 => self.op_5xy0(),
             0x6 => self.op_6xnn(),
             0x7 => self.op_7xnn(),
             0x8 => match self.current_op.3 {
@@ -196,7 +196,7 @@ impl Cpu {
         addr
     }
 
-    fn op_00ee(&mut self) {
+    fn op_00ee(&mut self) { // return from subroutine
         match self.stack.pop() {
             Some(return_val) => self.pc = return_val,
             None => println!("Error executing 0x00ee: nothing on call stack!"),
@@ -219,7 +219,7 @@ impl Cpu {
     fn op_4xnn(&mut self) {
     }
 
-    fn op_5xyn(&mut self) {
+    fn op_5xy0(&mut self) {
 
     }
 
@@ -429,4 +429,124 @@ mod cpu_tests {
         let addr = cpu.construct_address_from_op();
         assert_eq!(addr, 0x0123);
     }
+    
+    #[test]
+    fn test_op_2nnn_3xnn_4xnn_5xy0() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (0x2,0x2,0x2,0x2);
+        cpu.op_2nnn();
+        assert_eq!(cpu.stack[0], 0x0222);
+        assert!( cpu.stack.len() == 1 );
+
+        cpu.reset();
+        let op1 = (0x3,0x1,0x2,0x2);
+        let op2 = (0x3,0x1,0x4,0x4);
+
+        cpu.v[0x1] = 0x44;
+        cpu.v[0xE] = 0x43;
+        cpu.current_op = op1;
+        cpu.op_3xnn(); // since v[1] == 0x44, shouldn't skip.
+        assert_eq!(cpu.pc, 0x200);
+        cpu.op_4xnn(); // since v[1] != 0x22, should skip.
+        assert_eq!(cpu.pc, 0x202);
+
+        cpu.current_op = op2;
+        cpu.op_3xnn(); // v1 == NN, skip
+        assert_eq!(cpu.pc, 0x204);
+        cpu.op_4xnn(); // v1 == NN, don't skip
+        assert_eq!(cpu.pc, 0x204);
+
+        cpu.current_op = (0x5,0x1,0xE,0x0);
+        cpu.op_5xy0();
+        assert_eq!(cpu.pc, 0x204);
+        cpu.v[0xE] = 0x44;
+        cpu.op_5xy0();
+        assert_eq!(cpu.pc, 0x206);
+    }
+
+    #[test]
+    fn test_op_6xnn() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (0x6, 0xD, 0xA, 0xD);
+        cpu.op_6xnn();
+        assert_eq!(cpu.v[0xD], 0xAD);
+    }
+
+    #[test]
+    fn test_op_7xnn() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (0x7,0x2,0x0,0x3);
+        cpu.v[2] = 3;
+        let vf_state = cpu.v[0xF];
+        cpu.op_7xnn();
+        assert_eq!(cpu.v[2],6);
+        cpu.v[2] = 253;
+        cpu.op_7xnn();
+        assert_eq!(cpu.v[2],0); // we should have overflowed here
+        assert_eq!(cpu.v[0xF],vf_state); //...but carry shouldn't be affected
+    }
+
+    #[test]
+    fn test_op_8xy0() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (8,0,1,0);
+        cpu.v[0] = 40;
+        cpu.v[1] = 0;
+        cpu.op_8xy0();
+        assert_eq!(cpu.v[0], cpu.v[1]);
+    }
+
+    #[test]
+    fn test_op_8xy1() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (8,0,1,1);
+        cpu.v[0] = 0x25;
+        cpu.v[1] = 0x30;
+        cpu.op_8xy1();
+        assert_eq!(cpu.v[0], 0x35);
+    }
+
+    #[test]
+    fn test_op_8xy2() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (8,0,1,2);
+        cpu.v[0] = 0x25;
+        cpu.v[1] = 0x30;
+        cpu.op_8xy2();
+        assert_eq!(cpu.v[0], 0x20);
+    }
+
+    #[test]
+    fn test_op_8xy3() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (8,0,1,3);
+        cpu.v[0] = 0x25;
+        cpu.v[1] = 0x30;
+        cpu.op_8xy3();
+        assert_eq!(cpu.v[0], 0x15);
+    }
+
+    #[test]
+    fn test_op_8xy4() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (8,0,1,4);
+        cpu.v[0] = 250;
+        cpu.v[1] = 3;
+        cpu.op_8xy4();
+        assert_eq!(cpu.v[0], 253);
+        assert_eq!(cpu.v[0xF], 0);
+        cpu.op_8xy4();
+        assert_eq!(cpu.v[0], 0); //should have carried here
+        assert_eq!(cpu.v[0], 1); //...and set the carry flag
+    }
+
+    #[test]
+    fn test_op_8xy5() {
+        let mut cpu = Cpu::new();
+        cpu.current_op = (8,0,1,5);
+        cpu.v[0] = 3;
+        cpu.v[1] = 3;
+        cpu.op_8xy5();
+    }
+
 }
