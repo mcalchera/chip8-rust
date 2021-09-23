b8548c25d86dff114f6533db784f27c102b1aab2
Mark Calchera <mcalchera@gmail.com>
2021-09-23 11:44:59 -0500

    implement op 8xy3

--- src/cpu.rs
+++ src/cpu.rs
@@ -281,7 +281,12 @@ impl Cpu {
     }
 
     fn op_8xy3(&mut self) {
+        let x = self.current_op.1 as usize;
+        let y = self.current_op.2 as usize;
+
+        self.v[x] = self.v[x] ^ self.v[y];
     }
+
 
     fn op_8xy4(&mut self) {
     }
