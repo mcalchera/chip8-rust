ddc5af9029de7f1a341797463c627b46f1cd639c
Mark Calchera <mcalchera@gmail.com>
2021-09-24 16:44:31 -0500

    add config object and ROM loading driver code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 mod cpu;
 use crate::cpu::Cpu;
+use std::env;
+
+struct Config {
+    pub rom: String,
+}
+
+fn process_args(args: &[String]) -> Config {
+    if args.len() != 2 {
+        println!("Usage: {} <rom file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let fname = &args[1];
+
+    Config {
+        rom: fname.to_string(),
+    }
+}
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    println!("Hello, world!");
     let mut cpu = Cpu::new();
-    assert!(cpu.load_rom("/home/mcalchera/development/rust/chip8-rust/test_opcode.ch8").is_ok());
-    println!("{:?}",cpu);
+    let args: Vec<String> = env::args().collect(); 
+    let config = process_args(&args);
+
+    if !cpu.load_rom(config.rom.as_str()).is_ok() {
+        println!("chip8: error loading ROM file: \"{}\"", config.rom);
+        std::process::exit(1);
+    }
+
+    
 }
